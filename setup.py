8a714f6f341150d0bfc738d4bb4ecc270bb658a4
Пользователь <mninifu@gmail.com>
2021-02-23 15:34:53 +0200

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='json-navigator',
+      version='1.0',
+      description='Navigation through json file',
+      author='Nadia Zaiachkovska',
+      author_email='mninifu@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
